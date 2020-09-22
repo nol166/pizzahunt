439dda50d4696aa57863babcba6860288ec63dad
John McCambridge <jmccamb@gmail.com>
2020-09-21 19:52:25 -0500

    Add comment model to the pizza app

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,22 +1,45 @@
-const { Schema, model } = require('mongoose')
+const { Schema, model } = require('mongoose');
+const moment = require('moment');
 
-const PizzaSchema = new Schema({
+const PizzaSchema = new Schema(
+  {
     pizzaName: {
-        type: String,
+      type: String,
     },
     createdBy: {
-        type: String,
+      type: String,
     },
     createdAt: {
-        type: Date,
-        default: Date.now,
+      type: Date,
+      default: Date.now,
+      get: createdAtVal =>
+        moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a'),
     },
     size: {
-        type: String,
-        default: 'Large',
+      type: String,
+      default: 'Large',
     },
     toppings: [],
-})
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Comment',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function () {
+  return this.comments.length;
+});
 
 // create the Pizza model using the PizzaSchema
 const Pizza = model('Pizza', PizzaSchema);
